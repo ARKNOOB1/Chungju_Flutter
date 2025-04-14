53985b1f9d22e0d2c22e16a086d9a5745e953a3b
SMART24\User <a59114154@gmail.com>
2025-04-14 14:43:16 +0900

    fourth

--- lib/ToDoPage.dart
+++ lib/ToDoPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:untitled/drawer.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -8,12 +10,64 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+
+  final List<String> todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadTodos();
+  }
+
+  void _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getStringList("todos");
+    if(saved != null){
+      setState(() {
+        todos.addAll(saved);
+      });
+    }
+  }
+
+  void _deleteTodo(int index) async {
+    final removed = todos[index];
+    setState(() {
+      todos.removeAt(index);
+    });
+    _saveTodos();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('${removed} 삭제됨'))
+      );
+  }
+
+  void _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('todos', todos);
+  }
+
+
+  void _addTodo(String text){
+    if (text.trim().isEmpty) return;
+
+    setState(() {
+      todos.add(text.trim());
+      _controller.clear();
+    });
+    _saveTodos();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("To Do 앱"),
       ),
+      drawer: const MyDrawer(),
       body: Column(
         children: [
           Padding(
@@ -22,6 +76,7 @@ class _TodoPageState extends State<TodoPage> {
               children: [
                 Expanded(
                     child: TextField(
+                      controller: _controller,
                       decoration: InputDecoration(
                         hintText: "할 일을 입력하세요.",
                         border: OutlineInputBorder(
@@ -34,15 +89,35 @@ class _TodoPageState extends State<TodoPage> {
                 ),
                 const SizedBox(width: 10),
                 ElevatedButton(
-                    onPressed: () => {},
+                    onPressed: () => _addTodo(_controller.text),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.indigo,
                       foregroundColor: Colors.white
                     ),
                     child: const Text("추가")
-                )
+                ),
+
               ],
             ),
+          ),
+          Expanded(
+              child: todos.isEmpty
+                  ? const Center(child: Text("할 일이 없습니다."))
+                  : ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 4
+                      ),
+                      child: ListTile(
+                        title: Text(todos[index]),
+                        onLongPress: () => _deleteTodo(index),
+                      ),
+                    );
+                  }
+              )
           )
         ],
       ),
